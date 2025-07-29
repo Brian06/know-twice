9e09b1d37fc9ce0b0f69726cbdd1083e44ac6dfc
Brian Salazar <bsalazarsanchez06@gmail.com>
2025-07-28 19:31:37 -0600

    adding other full images

--- src/const/members.ts
+++ src/const/members.ts
@@ -3,38 +3,38 @@ import type { TMember } from "../types/members";
 export const members: TMember[] = [
   {
     name: "nayeon",
-    memberCardClasses: "hover:border-nayeon hover:bg-nayeon",
+    memberCardClasses: "hover:bg-nayeon",
   },
   {
     name: "jeongyeon",
-    memberCardClasses: "hover:border-jeongyeon hover:bg-jeongyeon",
+    memberCardClasses: "hover:bg-jeongyeon",
   },
   {
     name: "momo",
-    memberCardClasses: "hover:border-momo hover:bg-momo",
+    memberCardClasses: "hover:bg-momo",
   },
   {
     name: "sana",
-    memberCardClasses: "hover:border-sana hover:bg-sana",
+    memberCardClasses: "hover:bg-sana",
   },
   {
     name: "jihyo",
-    memberCardClasses: "hover:border-jihyo hover:bg-jihyo",
+    memberCardClasses: "hover:bg-jihyo",
   },
   {
     name: "mina",
-    memberCardClasses: "hover:border-mina hover:bg-mina",
+    memberCardClasses: "hover:bg-mina",
   },
   {
     name: "dahyun",
-    memberCardClasses: "hover:border-dahyun hover:bg-dahyun",
+    memberCardClasses: "hover:bg-dahyun",
   },
   {
     name: "chaeyoung",
-    memberCardClasses: "hover:border-chaeyoung hover:bg-chaeyoung",
+    memberCardClasses: "hover:bg-chaeyoung",
   },
   {
     name: "tzuyu",
-    memberCardClasses: "hover:border-tzuyu hover:bg-tzuyu",
+    memberCardClasses: "hover:bg-tzuyu",
   },
 ];
