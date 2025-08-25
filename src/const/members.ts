118e45bcafb76e022d3a5a2f35ce8f565a5d45bd
Brian Salazar <bsalazarsanchez06@gmail.com>
2025-08-24 18:42:32 -0600

    working on member pages

--- src/const/members.ts
+++ src/const/members.ts
@@ -1,40 +1,74 @@
 import type { TMember } from "../types/members";
 
-export const members: TMember[] = [
+export const MEMBERS: TMember[] = [
   {
-    name: "nayeon",
+    id: "nayeon",
+    name: "Nayeon",
+    fullName: "Im Nayeon",
+    emoji: "🐰",
+    instagram: {
+      username: "nayeonyny",
+      url: "https://www.instagram.com/nayeonyny/",
+    },
+    nationality: "South Korean",
+    birthday: new Date("1995-09-22"),
+    positions: ["Lead Vocalist", "Center", "GroupFace"],
+    songs: [
+      {
+        title: "POP!",
+        youtube: "https://youtu.be/f6YDKF0LVWw?si=lVo3cXGAHFhGdDMF",
+        spotify:
+          "https://open.spotify.com/track/3lOMJTQTd6J34faYwASc33?si=7d2fc6f03dee4e03",
+        appleMusic: "https://music.apple.com/us/song/pop/1624932774",
+      },
+    ],
+    funFacts: [
+      "Her height is 163cm",
+      "Her weight is 47kg",
+      "She has a younger sister, Seoyeon, born 1998.",
+      "She chose 9 as her favorite number, because TWICE has 9 members",
+      "Her left leg is weak due to her being in a car accident in her childhood.",
+    ],
     memberCardClasses: "hover:bg-nayeon",
   },
   {
-    name: "jeongyeon",
+    id: "jeongyeon",
+    name: "Jeongyeon",
     memberCardClasses: "hover:bg-jeongyeon",
   },
   {
-    name: "momo",
+    id: "momo",
+    name: "Momo",
     memberCardClasses: "hover:bg-momo",
   },
   {
-    name: "sana",
+    id: "sana",
+    name: "Sana",
     memberCardClasses: "hover:bg-sana",
   },
   {
-    name: "jihyo",
+    id: "jihyo",
+    name: "Jihyo",
     memberCardClasses: "hover:bg-jihyo",
   },
   {
-    name: "mina",
+    id: "mina",
+    name: "Mina",
     memberCardClasses: "hover:bg-mina",
   },
   {
-    name: "dahyun",
+    id: "dahyun",
+    name: "Dahyun",
     memberCardClasses: "hover:bg-dahyun",
   },
   {
-    name: "chaeyoung",
+    id: "chaeyoung",
+    name: "Chaeyoung",
     memberCardClasses: "hover:bg-chaeyoung",
   },
   {
-    name: "tzuyu",
+    id: "tzuyu",
+    name: "Tzuyu",
     memberCardClasses: "hover:bg-tzuyu",
   },
 ];
--- src/types/members.ts
+++ src/types/members.ts
@@ -1,4 +1,21 @@
 export type TMember = {
+  id: string;
   name: string;
+  fullName: string;
+  emoji: string;
+  instagram: {
+    username: string;
+    url: string;
+  };
+  nationality: string;
+  birthday: Date;
+  positions: string[];
+  songs: {
+    title: string;
+    youtube: string;
+    spotify: string;
+    appleMusic: string;
+  }[];
+  funFacts: string[];
   memberCardClasses: string;
 };
