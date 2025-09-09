a481067b3671ee494db9c10482b5035ae692628c
Brian Salazar <bsalazarsanchez06@gmail.com>
2025-09-08 21:09:42 -0600

    add member info

--- src/types/members.ts
+++ src/types/members.ts
@@ -1,21 +1,78 @@
+export type TSocial = {
+  platform: string;
+  username: string;
+  url: string;
+};
+
+export type TMusicLink = {
+  platform: string;
+  url: string;
+};
+
+export type TSong = {
+  title: string;
+  links: TMusicLink[];
+};
+
+export type Tnationality = "South Korean" | "Japanese" | "Taiwanese";
+
+export type Tname =
+  | "Nayeon"
+  | "Jeongyeon"
+  | "Momo"
+  | "Sana"
+  | "Jihyo"
+  | "Mina"
+  | "Dahyun"
+  | "Chaeyoung"
+  | "Tzuyu";
+
+export type TfullName =
+  | "Im Nayeon"
+  | "Yoo Jeong-yeon"
+  | "Hirai Momo"
+  | "Minatozaki Sana"
+  | "Park Ji-hyo"
+  | "Myoui Mina"
+  | "Kim Da-hyun"
+  | "Son Chae-young"
+  | "Chou Tzuyu";
+
+export type Temoji =
+  | "🐰"
+  | "🐶"
+  | "🍑"
+  | "🐭"
+  | "🦄"
+  | "🐧"
+  | "💛"
+  | "🐼"
+  | "🍓"
+  | "🦌";
+
+export type Tposition =
+  | "Main Vocalist"
+  | "Main Dancer"
+  | "Visual"
+  | "Center"
+  | "Group Face"
+  | "Sub-Vocalist"
+  | "Sub-Rapper"
+  | "Leader"
+  | "Main-Rapper"
+  | "Maknae";
+
 export type TMember = {
   id: string;
-  name: string;
-  fullName: string;
-  emoji: string;
-  instagram: {
-    username: string;
-    url: string;
-  };
-  nationality: string;
+  name: Tname;
+  fullName: TfullName;
+  emoji: Temoji;
+  color: string;
+  social: TSocial[];
+  nationality: Tnationality;
   birthday: Date;
-  positions: string[];
-  songs: {
-    title: string;
-    youtube: string;
-    spotify: string;
-    appleMusic: string;
-  }[];
+  positions: Tposition[];
+  songs: TSong[];
   funFacts: string[];
   memberCardClasses: string;
 };
