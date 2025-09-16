7be76b5eeb3216dc0cd950b397ee06c018755c2e
Brian Salazar <bsalazarsanchez06@gmail.com>
2025-09-15 18:16:27 -0600

    add transitions between pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // output: 'server',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  build: {
+    inlineStylesheets: 'always',
+  },
 });
