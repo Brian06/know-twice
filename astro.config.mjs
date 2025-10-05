731198f2d4bd4078ad60ad9e28af1a2bd0fc1e45
Brian Salazar <bsalazarsanchez06@gmail.com>
2025-10-04 20:25:42 -0600

    Deploy Improvements (#1)
    * deploy improvements
    
    * deploy improvements
    
    * deploy improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   // output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
+
   build: {
     inlineStylesheets: 'always',
   },
+
+  integrations: [sitemap()],
+  site: 'https://know-twice.vercel.app/',
 });
